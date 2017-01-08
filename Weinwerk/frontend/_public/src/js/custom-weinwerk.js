02886862b12c1f68e01ef3817a23c5ca360c9db1
Kevinschmid86 <Kevin_Jaeck@web.de>
2017-01-08 13:31:52 +0100

    Integrate age check with javascript

--- Weinwerk/frontend/_public/src/js/custom-weinwerk.js
+++ Weinwerk/frontend/_public/src/js/custom-weinwerk.js
@@ -1,35 +1,37 @@
-/**
- * Created by kevin.schmid on 02.10.2016.
- */
-
 //Shopware Modal for cookie question
-// check if cookie exist
-if(!Cookies.get('of_agel')) {
-    // if not open the modal
-    $.modal.open('<div style="padding: 0.5rem;">'+
-        '<p>Wein-Wolff tritt für einen verantwortungsvollen Genuss alkoholischer Getränke ab 18 Jahren ein. Bitte betreten Sie diese Website nicht, wenn Sie jünger als 18 Jahre sind.</p>'+
-        '<button class="btn is--primary cookie-ok">Ja, Ich bin 18 Jahre oder älter</button>'+
-        '<button class="btn is--secondary cookie-not-ok">Nein, Ich bin noch keine 18 Jahre</button>'+
-        '</div>',{
-        additionalClass: 'of_age',
-        width: 600,
-        height:225,
-        closeOnOverlay: false,
-        showCloseButton: false,
+//Get current url
+var current_url = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+//Check if checkout is in url
+if (current_url.search('checkout') > 0)
+{
+    // check if cookie exist
+    if(!Cookies.get('of_agel')) {
+        // if not open the modal
+        $.modal.open('<div style="padding: 0.5rem;">'+
+            '<p>Wein-Wolff tritt für einen verantwortungsvollen Genuss alkoholischer Getränke ab 18 Jahren ein. Bitte betreten Sie diese Website nicht, wenn Sie jünger als 18 Jahre sind.</p>'+
+            '<button class="btn is--primary cookie-ok">Ja, Ich bin 18 Jahre oder älter</button>'+
+            '<button class="btn is--secondary cookie-not-ok">Nein, Ich bin noch keine 18 Jahre</button>'+
+            '</div>',{
+            additionalClass: 'of_age',
+            width: 600,
+            height:225,
+            closeOnOverlay: false,
+            showCloseButton: false,
+        });
+    }
+
+// trigger the function if button is clicked
+    $('.of_age .cookie-ok').on('click', function() {
+        // Set the cookie
+        Cookies.set('of_agel', true, { expires: 7 });
+        // and close the modal
+        $.modal.close();
+    });
+
+// trigger the function if button is clicked
+    $('.of_age .cookie-not-ok').on('click', function() {
+        // Set the cookie
+        window.open('http://www.kenn-dein-limit.info/','_self');
     });
 }
-
-// trigger the function if button is clicked
-$('.of_age .cookie-ok').on('click', function() {
-    // Set the cookie
-    Cookies.set('of_agel', true, { expires: 7 });
-    // and close the modal
-    $.modal.close();
-});
-
-// trigger the function if button is clicked
-$('.of_age .cookie-not-ok').on('click', function() {
-    // Set the cookie
-    window.open('http://www.kenn-dein-limit.info/','_self');
-});
 
